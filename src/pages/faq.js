d36187cf3b74aa0bb7abca37ebcceeef2b47d589
18-12847 <jae2933@naver.com>
2023-06-22 00:31:30 +0900

    FAQ 페이지 틀 구성

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,6 +1,8 @@
-//FAQ 페이지
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Myheader from "../components/header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Accordion } from "react-bootstrap";
+import "../style/accordion.css"
 
 const FAQ = () => {
   const session = localStorage.getItem("sessionId");
@@ -13,15 +15,84 @@ const FAQ = () => {
   }, []);
 
   return (
-    <div className="faq">
+    <div>
       <Myheader isLogin={sessionId} />
-      <div>
-        <center>
-          <br /><br /><br />
-          <img src="https://img.freepik.com/premium-vector/faq-page-template-frequently-asked-question-banner-user-support-web-page-question-mark-and-pattern_668430-320.jpg" alt = "사진" width="1000px" />
-        </center>
+      <div style={{ width: '65%', marginTop: '60px', marginBottom: '10px', textAlign: 'center', margin: 'auto'}}>
+        <h1>Q & A</h1>
+      </div>
+      <div className="faq" style={{ width: '65%', marginTop: '0', display: "flex", justifyContent: "center", alignItems: "center", margin: 'auto' }}>
+        <div className="faq_accordion" style={{ width: "65%" }}>
+          <Accordion defaultActiveKey="0" className="mt-5 p-3">
+            <Accordion.Item eventKey="0" className="item">
+              <Accordion.Header>자주 묻는 질문1</Accordion.Header>
+              <Accordion.Body>
+                python
+                • Who wanted to go to the Snake Park in Nairobi and pay two shillings just to see a python?
+                • The voices were cut off as suddenly as the squeaks of a mouse were stifled by the lightning grip of his python.
+                • Along came a man carrying a large python.
+                • I found myself in front of the reticulated python.
+                • At first it appeared to be empty, but then, on the dappled ground, I saw a rock python.
+                • A tongue or a mouse's tail retracts within the lipless smile of a green tree python.
+                • Around the coast and in river valleys there are stretches of tropical jungle with pythons and other snakes.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2" className="item">
+              <Accordion.Header>자주 묻는 질문2</Accordion.Header>
+              <Accordion.Body>
+                python
+                • Who wanted to go to the Snake Park in Nairobi and pay two shillings just to see a python?
+                • The voices were cut off as suddenly as the squeaks of a mouse were stifled by the lightning grip of his python.
+                • Along came a man carrying a large python.
+                • I found myself in front of the reticulated python.
+                • At first it appeared to be empty, but then, on the dappled ground, I saw a rock python.
+                • A tongue or a mouse's tail retracts within the lipless smile of a green tree python.
+                • Around the coast and in river valleys there are stretches of tropical jungle with pythons and other snakes.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3" className="item">
+              <Accordion.Header>자주 묻는 질문3</Accordion.Header>
+              <Accordion.Body>
+                python
+                • Who wanted to go to the Snake Park in Nairobi and pay two shillings just to see a python?
+                • The voices were cut off as suddenly as the squeaks of a mouse were stifled by the lightning grip of his python.
+                • Along came a man carrying a large python.
+                • I found myself in front of the reticulated python.
+                • At first it appeared to be empty, but then, on the dappled ground, I saw a rock python.
+                • A tongue or a mouse's tail retracts within the lipless smile of a green tree python.
+                • Around the coast and in river valleys there are stretches of tropical jungle with pythons and other snakes.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="4" className="item">
+              <Accordion.Header>자주 묻는 질문4</Accordion.Header>
+              <Accordion.Body>
+                python
+                • Who wanted to go to the Snake Park in Nairobi and pay two shillings just to see a python?
+                • The voices were cut off as suddenly as the squeaks of a mouse were stifled by the lightning grip of his python.
+                • Along came a man carrying a large python.
+                • I found myself in front of the reticulated python.
+                • At first it appeared to be empty, but then, on the dappled ground, I saw a rock python.
+                • A tongue or a mouse's tail retracts within the lipless smile of a green tree python.
+                • Around the coast and in river valleys there are stretches of tropical jungle with pythons and other snakes.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="5" className="item">
+              <Accordion.Header>자주 묻는 질문5</Accordion.Header>
+              <Accordion.Body>
+                python
+                • Who wanted to go to the Snake Park in Nairobi and pay two shillings just to see a python?
+                • The voices were cut off as suddenly as the squeaks of a mouse were stifled by the lightning grip of his python.
+                • Along came a man carrying a large python.
+                • I found myself in front of the reticulated python.
+                • At first it appeared to be empty, but then, on the dappled ground, I saw a rock python.
+                • A tongue or a mouse's tail retracts within the lipless smile of a green tree python.
+                • Around the coast and in river valleys there are stretches of tropical jungle with pythons and other snakes.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </div>
       </div>
     </div>
+
   );
 };
 
