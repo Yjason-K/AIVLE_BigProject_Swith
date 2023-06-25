1b07485b50f393f151230e7ed68e8dfe2fdd086f
Yjason-K <gomgom0324@gmail.com>
2023-06-26 00:03:47 +0900

    style - aivle.js (card flip animation)

--- src/pages/aivle.js
+++ src/pages/aivle.js
@@ -10,13 +10,48 @@ import hwanjun from "../img/hwanjun.png";
 import hanjoon from "../img/hanjoon.png";
 
 const teamMembers = [
-  { name: "김용환", description: "AI 모델링", image: younghwan },
-  { name: "장규진", description: "AI 모델링", image: gyujin },
-  { name: "조예령", description: "AI 모델링", image: yeryeong },
-  { name: "김영재", description: "Frontend", image: youngjae },
-  { name: "오승재", description: "Frontend", image: seungjae },
-  { name: "남환준", description: "Backend", image: hwanjun },
-  { name: "이한준", description: "Backend", image: hanjoon },
+  {
+    name: "김용환",
+    description: "AI 모델링",
+    image: younghwan,
+    git: "https://github.com/brightface",
+  },
+  {
+    name: "장규진",
+    description: "AI 모델링",
+    image: gyujin,
+    git: "https://github.com/kj021",
+  },
+  {
+    name: "조예령",
+    description: "AI 모델링",
+    image: yeryeong,
+    git: "https://github.com/CYeryeong",
+  },
+  {
+    name: "김영재",
+    description: "Frontend",
+    image: youngjae,
+    git: "https://github.com/Yjason-K",
+  },
+  {
+    name: "오승재",
+    description: "Frontend",
+    image: seungjae,
+    git: "https://github.com/18-12847",
+  },
+  {
+    name: "남환준",
+    description: "Backend",
+    image: hwanjun,
+    git: "https://github.com/only-juun",
+  },
+  {
+    name: "이한준",
+    description: "Backend",
+    image: hanjoon,
+    git: "https://github.com/leehanjun506",
+  },
 ];
 
 const AIVLE = () => {
@@ -42,14 +77,30 @@ const AIVLE = () => {
           {teamMembers.slice(0, 3).map((member, index) => (
             <div key={index} className="team-card">
               <div className="card">
-                <img
-                  src={member.image}
-                  className="card-img-top"
-                  alt="team member"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{member.name}</h5>
-                  <p className="card-text">{member.description}</p>
+                <div className="card-inner">
+                  <div className="card-front">
+                    <img
+                      src={member.image}
+                      className="card-img-top"
+                      alt="team member"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{member.name}</h5>
+                      <p className="card-text">{member.description}</p>
+                    </div>
+                  </div>
+                  <div className="card-back">
+                    <div className="card-back-content">
+                      <a
+                        href={member.git}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="github-link"
+                      >
+                        GitHub
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -59,14 +110,30 @@ const AIVLE = () => {
           {teamMembers.slice(3).map((member, index) => (
             <div key={index} className="team-card">
               <div className="card">
-                <img
-                  src={member.image}
-                  className="card-img-top"
-                  alt="team member"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{member.name}</h5>
-                  <p className="card-text">{member.description}</p>
+                <div className="card-inner">
+                  <div className="card-front">
+                    <img
+                      src={member.image}
+                      className="card-img-top"
+                      alt="team member"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{member.name}</h5>
+                      <p className="card-text">{member.description}</p>
+                    </div>
+                  </div>
+                  <div className="card-back">
+                    <div className="card-back-content">
+                      <a
+                        href={member.git}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="github-link"
+                      >
+                        GitHub
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
--- src/pages/faq.js
+++ src/pages/faq.js
@@ -70,78 +70,60 @@ const FAQ = () => {
           <div className="faq_accordion" style={{ width: "80%" }}>
             <Accordion defaultActiveKey="0" className="mt-5 p-3">
               <Accordion.Item eventKey="0" className="item">
-                <Accordion.Header>자주 묻는 질문1</Accordion.Header>
+                <Accordion.Header>
+                  카메라 찰영 관련해서 사생활 침해 문제는 없을까요?
+                </Accordion.Header>
                 <Accordion.Body>
-                  python • Who wanted to go to the Snake Park in Nairobi and pay
-                  two shillings just to see a python? • The voices were cut off
-                  as suddenly as the squeaks of a mouse were stifled by the
-                  lightning grip of his python. • Along came a man carrying a
-                  large python. • I found myself in front of the reticulated
-                  python. • At first it appeared to be empty, but then, on the
-                  dappled ground, I saw a rock python. • A tongue or a mouse's
-                  tail retracts within the lipless smile of a green tree python.
-                  • Around the coast and in river valleys there are stretches of
-                  tropical jungle with pythons and other snakes.
+                  저희 서비스는 고객의 개인 정보와 사생활을 보호하기 위해 영상을
+                  어디에도 저장하지 않습니다. 카메라가 작동하는 동안 촬영된
+                  영상은 실시간으로만 전송되고 저장되지 않습니다. 로그에서 볼 수
+                  있는 사진은 이상 현상을 감지할 때만 기록이 생성됩니다. 이
+                  경우, 해당 이벤트가 발생한 시간의 사진 또는 기록만이 생성되며,
+                  그 외에는 아무런 정보도 기록되지 않습니다.
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="2" className="item">
-                <Accordion.Header>자주 묻는 질문2</Accordion.Header>
+                <Accordion.Header>화면이 멈췄어요.</Accordion.Header>
                 <Accordion.Body>
-                  python • Who wanted to go to the Snake Park in Nairobi and pay
-                  two shillings just to see a python? • The voices were cut off
-                  as suddenly as the squeaks of a mouse were stifled by the
-                  lightning grip of his python. • Along came a man carrying a
-                  large python. • I found myself in front of the reticulated
-                  python. • At first it appeared to be empty, but then, on the
-                  dappled ground, I saw a rock python. • A tongue or a mouse's
-                  tail retracts within the lipless smile of a green tree python.
-                  • Around the coast and in river valleys there are stretches of
-                  tropical jungle with pythons and other snakes.
+                  인터넷 연결이 불안정 하거나 카메라 전원에 이상이 있는 경우
+                  화면이 멈춰 있을수도 있습니다. 카메라와 연결된 와이파이 연결
+                  또는 카메라 전원이 정상적으로 연결되어 있는지 확인
+                  부탁드립니다. 이후에도 정상작동하지 않는 경우 1:1 문의를
+                  주시면 최대한 빠르게 연락드리겠습니다. 감사합니다.
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="3" className="item">
-                <Accordion.Header>자주 묻는 질문3</Accordion.Header>
+                <Accordion.Header>서비스 성능이 궁금해요!</Accordion.Header>
                 <Accordion.Body>
-                  python • Who wanted to go to the Snake Park in Nairobi and pay
-                  two shillings just to see a python? • The voices were cut off
-                  as suddenly as the squeaks of a mouse were stifled by the
-                  lightning grip of his python. • Along came a man carrying a
-                  large python. • I found myself in front of the reticulated
-                  python. • At first it appeared to be empty, but then, on the
-                  dappled ground, I saw a rock python. • A tongue or a mouse's
-                  tail retracts within the lipless smile of a green tree python.
-                  • Around the coast and in river valleys there are stretches of
-                  tropical jungle with pythons and other snakes.
+                  S.with는 직접 개발한 딥러닝 알고리즘을 통하여 다양한
+                  위험패턴과 상항을 식별할 수 있도록 학습되어 있습니다. 저희
+                  AI모델은 지속적인 학습과 개선을 통해 성능을 향상시키고
+                  있습니다. 다른 서비스와 달리 wifi데이터를 카메라 데이터와 함께
+                  사용하여 더욱더 정확한 서비스를 제공합니다.
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="4" className="item">
-                <Accordion.Header>자주 묻는 질문4</Accordion.Header>
+                <Accordion.Header>
+                  카메라가 고장나서 새로운 카메라를 받는 경우에는 설정해야 하는
+                  부분이 있나요?
+                </Accordion.Header>
                 <Accordion.Body>
-                  python • Who wanted to go to the Snake Park in Nairobi and pay
-                  two shillings just to see a python? • The voices were cut off
-                  as suddenly as the squeaks of a mouse were stifled by the
-                  lightning grip of his python. • Along came a man carrying a
-                  large python. • I found myself in front of the reticulated
-                  python. • At first it appeared to be empty, but then, on the
-                  dappled ground, I saw a rock python. • A tongue or a mouse's
-                  tail retracts within the lipless smile of a green tree python.
-                  • Around the coast and in river valleys there are stretches of
-                  tropical jungle with pythons and other snakes.
+                  기기마다 고유의 시리얼 넘버를 가지고 있기 때문에 기기가
+                  변경되는 경우 Mypage에서 새로 받은 기기의 시리얼번호를
+                  등록해야 기존의 서비스를 계속해서 사용하실 수 있습니다. 추가
+                  문의 사항이 있으시면 1:1 문의로 연락드리면 자세히
+                  설명드리겠습니다.
                 </Accordion.Body>
               </Accordion.Item>
               <Accordion.Item eventKey="5" className="item">
-                <Accordion.Header>자주 묻는 질문5</Accordion.Header>
+                <Accordion.Header>
+                  연령대와 카메라 이용 시간대는 왜 입력해야 하나요?
+                </Accordion.Header>
                 <Accordion.Body>
-                  python • Who wanted to go to the Snake Park in Nairobi and pay
-                  two shillings just to see a python? • The voices were cut off
-                  as suddenly as the squeaks of a mouse were stifled by the
-                  lightning grip of his python. • Along came a man carrying a
-                  large python. • I found myself in front of the reticulated
-                  python. • At first it appeared to be empty, but then, on the
-                  dappled ground, I saw a rock python. • A tongue or a mouse's
-                  tail retracts within the lipless smile of a green tree python.
-                  • Around the coast and in river valleys there are stretches of
-                  tropical jungle with pythons and other snakes.
+                  연령대와 카메라 이용 시간대에 따라 AI가 감지해야할 위험 행동과
+                  비상상황 주의 시간대가 달라집니다. 이는 각 사용자에 최적화된
+                  안전 솔루션을 제공하기 위함이며 이러한 정보를 입력할 경우 보다
+                  높은 성능의 AI 서비스를 제공받으실 수 있습니다.
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
