ec61283b297ee412b60b346c7ce3a561a31b8e4f
18-12847 <jae2933@naver.com>
2023-06-30 10:25:13 +0900

    add - 실시간 스트리밍 백업

--- src/pages/service.js
+++ src/pages/service.js
@@ -1,4 +1,3 @@
-//서비스 페이지
 import { useEffect, useState } from "react";
 import Myheader from "../components/header";
 import "../style/service.css";
@@ -6,30 +5,54 @@ import { useNavigate } from "react-router-dom";
 
 const SERVICE = () => {
   const session = localStorage.getItem("sessionId");
-  const [sessionId, setSessionId] = useState(false);
+  const [sessionId, setSessionId] = useState(session && session !== "null");
+  const [showOverlay, setShowOverlay] = useState(true); // Overlay 상태
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (session && session !== "null") {
-      setSessionId(true);
-    }
-  }, []);
+    const timer = setTimeout(() => {
+      setShowOverlay(false); // 일정 시간이 지나면 레이어 제거
+    }, 10000);
 
-  if (!sessionId) {
-    navigate("/main", { replace: true });
-  }
+    return () => {
+      clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 클리어
+    };
+  }, []);
 
   return (
     <div className="service">
       <Myheader isLogin={sessionId} />
       <div className="container">
-        <div className="box livecam">라이브캠</div>
+        <div className="box livecam" style={{ position: 'relative' }}>
+          {showOverlay && 
+            <div className="overlay" 
+                 style={{ 
+                   backgroundColor: 'white', 
+                   position: 'absolute', 
+                   width: '100%', 
+                   height: '100%', 
+                   zIndex: 1 
+                 }}
+            ></div>
+          } 
+          <div style={{ pointerEvents: "none", zIndex: 0 }}>
+            <iframe
+              width="880"
+              height="495"
+              src="https://www.youtube.com/embed/hocye6hLKFo?autoplay=1&mute=1"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
         <div className="verticalContainer">
-          <div className="box logContainer">
+          {/* <div className="box logContainer">
             <div className="log_record">로그 기록</div>
             <div className="log_number">2</div>
           </div>
-          <div className="box service_function">서비스 기능</div>
+          <div className="box service_function">서비스 기능</div> */}
         </div>
       </div>
     </div>
